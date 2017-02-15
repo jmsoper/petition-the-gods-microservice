a6f383232fd8cf353e7362dbdb4d07af803af19e
jmsoper <jmsoper@gmail.com>
2017-02-15 16:21:35 -0500

    integrate calendar and response

--- index.js
+++ index.js
@@ -4,27 +4,26 @@ var calendar = require('./calendar');
 var portNumber = 3000;
 
 var server = http.createServer(function(request,response){
+    var calendarType = typeof calendar;
     var givenUrl = request.url.slice(1);
     var dateInfo = givenUrl.split("-");
-    var month = dateInfo[0];
-    var date = dateInfo[1];
-    // var holiday = calendar[month][date];
-    // var holidayText;
-    // if (calendar[month][date] != ""){
-    //   holidayText = calendar[month][date];
-    // } else {
-    //   holidayText = "no holiday today.";
-    // //   while (calendar[month][date].length == 0){
-    // //     if (date < calendar[month].length - 1){
-    // //       date += 1;
-    // //     } else if (month < 11){
-    // //       month += 1;
-    // //     } else {
-    // //       month = 1;
-    // //     }
-    // //   }
-    // //   holidayText = calendar[month][date];
-    // }
+    var month = parseInt(dateInfo[0]);
+    var date = parseInt(dateInfo[1]);
+    var holidayInfo = {
+      isAHoliday: true,
+      holidayText: ""
+    };
+    if (!calendar[month]){
+      response.end("not a valid month.");
+    } else if (!calendar[month][date]){
+      response.end("not a valid date.");
+    } else {
+      if (calendar[month][date] == "not a holiday"){
+        holidayInfo.isAHoliday = false;
+      }
+      holidayInfo.holidayText = calendar[month][date];
+    }
 
-    response.end(dateInfo.toString());
+    holidayInfo = JSON.stringify(holidayInfo);
+    response.end(holidayInfo);
 }).listen(portNumber);
